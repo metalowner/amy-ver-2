8e2f9eeeeaa72f6a7a8481995edda3e0da23994d
Pasha <pavelvini90@gmail.com>
2025-01-22 17:54:18 +0200

    new branch

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAaboroWx51bVpf2gn8X00S948wE3654MU',
+  authDomain: 'amy-ver-2.firebaseapp.com',
+  projectId: 'amy-ver-2',
+  storageBucket: 'amy-ver-2.firebasestorage.app',
+  messagingSenderId: '223332939584',
+  appId: '1:223332939584:web:041368e4ae5c59d18deca5',
+}
+
+firebase.initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
